eb6bbb95bf44c7c9ca7b73d5d5ab85db97a3a55c
DEBAIN Maxime <maxx@MacBook-Air.local>
2022-07-15 16:21:39 +0200

    page 404

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import './style.scss';
 
 const NotFound = () => {
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+        let path = `/`; 
+        navigate(path);
+    }
     return (
         <div className='container'>
             <h1>404</h1>
@@ -9,7 +15,7 @@ const NotFound = () => {
             How you got here is a mystery. But you can click the button below
             to go back to the homepage.
             </p>
-            <button className="btn white">HOME</button>
+            <button className="btn white" onClick={routeChange}>HOME</button>
         </div>
     );
 };
